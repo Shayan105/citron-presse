f250b85b6939477dcad9a2d6e4bd3be6a584bbdd
Shayan105 <119674649+Shayan105@users.noreply.github.com>
2025-03-18 17:42:23 +0100

    radioDl + front

---
+++ serv.py
@@ -0,0 +1,86 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import socketserver
+import os
+import json
+import html
+
+baseFilePath = "index.html"
+
+
+IMAGE_HEADER_TAG = "image/jpeg"
+SONG_HEADER_TAG = "audio/mpeg"
+JSON_HEADER_TAG = "application/json"
+
+
+
+
+
+class Serv(BaseHTTPRequestHandler):
+    global profile
+    profile = 0
+    
+    def do_GET(self):
+
+        parsed = self.path.split("/")[1:]
+        print(parsed)
+        if self.path == '/':
+            self.path = '/' + baseFilePath
+
+        elif self.path == "/icon.png":
+
+
+
+        try:
+            file_to_open = open(self.path[1:]).read()
+            self.send_response(200)
+        except:
+            file_to_open = "File not found"
+            self.send_response(404)
+        self.end_headers()
+        self.wfile.write(bytes(file_to_open, 'utf-8'))
+
+
+
+
+
+
+
+
+
+
+    def send_File_content(self, filePath: str, contentType: str, chunk_size=8192, hasHeader = True ):
+        try:
+            with open(filePath, "rb") as file:
+                self.send_response(200)
+                if hasHeader:
+                    self.send_header('Content-type', contentType)
+                self.end_headers()
+                while chunk := file.read(chunk_size):
+                    self.wfile.write(chunk)
+        except FileNotFoundError:
+            self.send_response(404)
+            self.end_headers()
+            self.wfile.write(b"File not found")
+        except Exception as e:
+            self.send_response(500)
+            self.end_headers()
+            self.wfile.write(b"Internal Server Error")
+
+    def send_Json_content(self, jsonStr):
+        self.send_response(200)
+        self.send_header('Content-type', JSON_HEADER_TAG)
+        self.end_headers()
+        self.wfile.write(bytes(jsonStr, 'utf-8'))
+
+
+class ThreadedHTTPServer(socketserver.ThreadingMixIn, HTTPServer):
+    """Handle requests in a separate thread."""
+
+def run(server_class=ThreadedHTTPServer, handler_class=Serv, port=8080):
+    server_address = ('localhost', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Starting httpd server on port {port}')
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    run()
---
+++ song_dl.py
@@ -0,0 +1,58 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from time import sleep
+import soundcard as sc
+import soundfile as sf
+from datetime import datetime
+import threading
+import schedule
+import time
+
+# Generate the output file name with the current date
+current_date = datetime.now().strftime("%d-%m-%Y")
+OUTPUT_FILE_NAME = f"{current_date}.wav"  # file name.
+SAMPLE_RATE = 48000  # [Hz]. sampling rate.
+
+DURATION = 7*60+35
+DELTA = 5
+
+def record_audio():
+    with sc.get_microphone(id=str(sc.default_speaker().name), include_loopback=True).recorder(samplerate=SAMPLE_RATE) as mic:
+        # record audio with loopback from default speaker.
+        data = mic.record(numframes=SAMPLE_RATE * DURATION)
+        
+        # change "data=data[:, 0]" to "data=data", if you would like to write audio as multiple-channels.
+        sf.write(file=OUTPUT_FILE_NAME, data=data[:, 0], samplerate=SAMPLE_RATE)
+
+def run_script():
+    # Initialize the WebDriver
+    driver = webdriver.Chrome()
+
+    # Start recording audio in a separate thread
+    audio_thread = threading.Thread(target=record_audio)
+    audio_thread.start()
+
+    driver.get("https://www.rfj.ch/rfj/Accueil/RFJ-votre-radio-regionale.html")
+    sleep(DELTA)
+    driver.find_element(By.CSS_SELECTOR, ".manual-link > .fa-volume-up").click()
+    sleep(DURATION)
+
+    driver.close()
+
+    # Wait for the audio recording to finish
+    audio_thread.join()
+
+# Schedule the script to run every day at 11:58 AM except on Saturdays and Sundays
+def job():
+    if datetime.now().weekday() < 5:  # Monday to Friday are 0-4
+        run_script()
+
+schedule.every().day.at("11:48").do(job)
+
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+
+
